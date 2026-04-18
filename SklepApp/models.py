a98319b68b44496852a03d0439794097ca432557
Muchabzz <kamil26082005@wp.pl>
2026-04-18 13:53:04 +0200

    zdjęcia
    utworzono możliwość dodawania zdjęć do produktów

--- SklepApp/models.py
+++ SklepApp/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     availability = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
 
     def __str__(self):
         return self.name
