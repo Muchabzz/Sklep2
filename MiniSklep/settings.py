829a2c6eda9684aec4b813bd60c431fc2ab935f4
Muchabzz <kamil26082005@wp.pl>
2026-04-26 17:42:18 +0200

    static and imagines fix

--- MiniSklep/settings.py
+++ MiniSklep/settings.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
